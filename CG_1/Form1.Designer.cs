989a1ad648a24a59a38ce0178f95fb9f4f84f1d9
ShkulevMaksim <79005304+ShkulevMaksim@users.noreply.github.com>
2022-05-25 19:16:24 +0700

    Initial commit

---
+++ CG_1/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace CG_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_up = new System.Windows.Forms.Button();
+            this.button_left = new System.Windows.Forms.Button();
+            this.button_down = new System.Windows.Forms.Button();
+            this.button_right = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(638, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(702, 32);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(87, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(647, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Scale:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(656, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Rotate x";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(656, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Rotate y";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(656, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Rotate z";
+            // 
+            // button_up
+            // 
+            this.button_up.Location = new System.Drawing.Point(717, 317);
+            this.button_up.Name = "button_up";
+            this.button_up.Size = new System.Drawing.Size(33, 31);
+            this.button_up.TabIndex = 11;
+            this.button_up.Text = "🡅";
+            this.button_up.UseVisualStyleBackColor = true;
+            this.button_up.Click += new System.EventHandler(this.button_up_Click);
+            // 
+            // button_left
+            // 
+            this.button_left.Location = new System.Drawing.Point(673, 354);
+            this.button_left.Name = "button_left";
+            this.button_left.Size = new System.Drawing.Size(33, 31);
+            this.button_left.TabIndex = 12;
+            this.button_left.Text = "🡄";
+            this.button_left.UseVisualStyleBackColor = true;
+            this.button_left.Click += new System.EventHandler(this.button_left_Click);
+            // 
+            // button_down
+            // 
+            this.button_down.Location = new System.Drawing.Point(717, 389);
+            this.button_down.Name = "button_down";
+            this.button_down.Size = new System.Drawing.Size(33, 31);
+            this.button_down.TabIndex = 13;
+            this.button_down.Text = "🡇";
+            this.button_down.UseVisualStyleBackColor = true;
+            this.button_down.Click += new System.EventHandler(this.button_down_Click);
+            // 
+            // button_right
+            // 
+            this.button_right.Location = new System.Drawing.Point(756, 354);
+            this.button_right.Name = "button_right";
+            this.button_right.Size = new System.Drawing.Size(33, 31);
+            this.button_right.TabIndex = 14;
+            this.button_right.Text = "🡆";
+            this.button_right.UseVisualStyleBackColor = true;
+            this.button_right.Click += new System.EventHandler(this.button_right_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(712, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 31);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "↺";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(755, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 31);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "↻";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(712, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(33, 31);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "↺";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(756, 206);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(33, 31);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "↻";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(712, 257);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(33, 31);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "↺";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(756, 256);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(33, 31);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "↻";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_right);
+            this.Controls.Add(this.button_down);
+            this.Controls.Add(this.button_left);
+            this.Controls.Add(this.button_up);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Button button_up;
+        private System.Windows.Forms.Button button_left;
+        private System.Windows.Forms.Button button_down;
+        private System.Windows.Forms.Button button_right;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TrackBar trackBar1;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        #endregion
+    }
+}
---
+++ CG_1/ОТЧЕТ/Form1.cs
@@ -0,0 +1,463 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CG_1
+
+{
+    public struct Vertex
+    {
+        public double X { set; get; }
+        public double Y { set; get; }
+        public double Z { set; get; }
+    
+        public Vertex(double x, double y, double z) : this() {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+
+    public struct Polygon
+    {
+        public int X { set; get; }
+        public int Y { set; get; }
+        public int Z { set; get; }
+        public int W { set; get; }
+    
+        public Polygon(int x, int y, int z,int w) : this()
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+            this.W = w;
+        }
+    }
+    
+    public partial class Form1 : Form
+    {
+        private int _scale = 50;
+
+        private readonly List<Polygon> _polygons;
+        private List<Vertex> _vertices;
+        private double[,] _verticiesMatrix;
+        private readonly Pen _myPen;
+        private Vertex _vertex;
+
+        private double alpha = 0;
+
+        private int totalDeltaX=0;
+        private int totalDeltaY=0;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _myPen = new Pen(Color.Black,3);
+            
+            var lines = File.ReadAllLines(@"cube3.obj");
+            _vertices = new List<Vertex>();  
+            _polygons = new List<Polygon>(); 
+            foreach (var line in lines) {
+                if (line.ToLower().StartsWith("v ")) {
+                    var vx = line.Split(' ')
+                        .Skip(1)
+                        .Select(v => double.Parse(v.Replace('.', ',')))
+                        .ToArray();
+                    _vertices.Add(new Vertex(vx[0], vx[1], vx[2]));
+                }
+            
+                else if (line.ToLower().StartsWith("f")) {
+                    var vx = line.Split(' ')
+                        .Skip(1)
+                        .Select(int.Parse)
+                        .ToArray();
+                    _polygons.Add(new Polygon(vx[0]-1, vx[1]-1, vx[2]-1,vx[3]-1));
+                }
+            }
+
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+
+        }
+
+        private void DrawPolygon(Polygon polygon,PaintEventArgs e)
+        {
+            
+            e.Graphics.DrawLine(_myPen, (float)_vertices[polygon.X].X*_scale,(float)_vertices[polygon.X].Y*_scale,
+                                     (float)_vertices[polygon.Y].X*_scale,(float)_vertices[polygon.Y].Y*_scale);
+            
+            e.Graphics.DrawLine(_myPen, (float)_vertices[polygon.Y].X*_scale,(float)_vertices[polygon.Y].Y*_scale,
+                                     (float)_vertices[polygon.Z].X*_scale,(float)_vertices[polygon.Z].Y*_scale);
+            
+            e.Graphics.DrawLine(_myPen, (float)_vertices[polygon.Z].X*_scale,(float)_vertices[polygon.Z].Y*_scale,
+                                        (float)_vertices[polygon.W].X*_scale,(float)_vertices[polygon.W].Y*_scale);
+            
+            e.Graphics.DrawLine(_myPen, (float)_vertices[polygon.W].X*_scale,(float)_vertices[polygon.W].Y*_scale,
+                                     (float)_vertices[polygon.X].X*_scale,(float)_vertices[polygon.X].Y*_scale);
+            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Azure);
+            foreach (var poly in _polygons)
+            {
+                DrawPolygon(poly,e);
+            }
+
+          
+        }
+
+
+        private double[,] Multiplication(double[,] matrixA, double[,] matrixB)
+        {
+            if (matrixA.GetLength(1) != matrixB.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+            var result = new double[matrixA.GetLength(0), matrixB.GetLength(1)];
+            for (var i = 0; i < matrixA.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrixB.GetLength(1); j++)
+                {
+                    for (var k = 0; k < matrixB.GetLength(0); k++)
+                    {
+                        result[i,j] += matrixA[i,k] * matrixB[k,j];
+                    }
+                }
+            }
+            return result;
+
+        }
+
+        private double[,] ConvertListToMatrix(List<Vertex> vertices)
+        {
+            var result = new double[vertices.Count,3];
+
+            for (var i = 0; i < vertices.Count; i++)
+            {
+                result[i, 0] = vertices[i].X;
+                result[i, 1] = vertices[i].Y;
+                result[i, 2] = vertices[i].Z;
+            }
+
+            return result;
+        }
+
+        private List<Vertex> ConvertMatrixToList(double[,] matrix)
+        {
+            var result = new List<Vertex>();
+
+            Vertex v;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                v = new Vertex(matrix[i, 0], matrix[i, 1], matrix[i, 2]);
+                result.Add(v);
+            }
+            
+            
+            return result;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            _scale = (trackBar1.Value+1)*50;
+            pictureBox1.Refresh();
+        }
+
+
+
+        private void button_down_Click(object sender, EventArgs e)
+        {
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            var dx = 1;
+            totalDeltaY += dx;
+
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 1] += dx;
+               
+            }
+
+
+            var result =ConvertMatrixToList( _verticiesMatrix);
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+        }
+
+        private void button_up_Click(object sender, EventArgs e)
+        {
+            
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            var dx = 1;
+            totalDeltaY -= dx;
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 1] -= dx;
+               
+            }
+
+
+            var result =ConvertMatrixToList( _verticiesMatrix);
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+        }
+
+        private void button_right_Click(object sender, EventArgs e)
+        {
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            var dy = 1;
+            totalDeltaX += dy;
+
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] += dy;
+            }
+
+
+            var result =ConvertMatrixToList( _verticiesMatrix);
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+        }
+
+        private void button_left_Click(object sender, EventArgs e)
+        {
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            var dy = 1;
+            totalDeltaX -= dy;
+
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] -= dy;
+            }
+
+
+            var result =ConvertMatrixToList( _verticiesMatrix);
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] -= totalDeltaX;
+                _verticiesMatrix[i, 1] -= totalDeltaY;
+            }
+
+            alpha += (double)3*Math.PI/180;
+            double[,] Mx =
+            {
+                {1, 0,0 },
+                {0,Math.Cos(alpha), -Math.Sin(alpha) },
+                {0, Math.Sin(alpha), Math.Cos(alpha) }
+            };
+
+            _verticiesMatrix= Multiplication(_verticiesMatrix, Mx);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] += totalDeltaX;
+                _verticiesMatrix[i, 1] += totalDeltaY;
+              
+               
+            }
+
+            var result = ConvertMatrixToList(_verticiesMatrix);
+
+           
+           
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] -= totalDeltaX;
+                _verticiesMatrix[i, 1] -= totalDeltaY;
+            }
+
+            alpha -= (double)3*Math.PI/180;
+            double[,] Mx =
+            {
+                {1, 0,0 },
+                {0,Math.Cos(alpha), -Math.Sin(alpha) },
+                {0, Math.Sin(alpha), Math.Cos(alpha) }
+            };
+
+            _verticiesMatrix= Multiplication(_verticiesMatrix, Mx);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] += totalDeltaX;
+                _verticiesMatrix[i, 1] += totalDeltaY;
+              
+               
+            }
+
+            var result = ConvertMatrixToList(_verticiesMatrix);
+
+           
+           
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+            
+       
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] -= totalDeltaX;
+                _verticiesMatrix[i, 1] -= totalDeltaY;
+            }
+            alpha -= (double)3*Math.PI/180;
+            double[,] Mx =
+            {
+                {Math.Cos(alpha), 0,Math.Sin(alpha) },
+                {0,1, 0 },
+                {-Math.Sin(alpha), 0, Math.Cos(alpha) }
+            };
+
+            _verticiesMatrix= Multiplication(_verticiesMatrix, Mx);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] += totalDeltaX;
+                _verticiesMatrix[i, 1] += totalDeltaY;
+              
+               
+            }
+
+            var result = ConvertMatrixToList(_verticiesMatrix);
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] -= totalDeltaX;
+                _verticiesMatrix[i, 1] -= totalDeltaY;
+            }
+            alpha += (double)3*Math.PI/180;
+            double[,] Mx =
+            {
+                {Math.Cos(alpha), 0,Math.Sin(alpha) },
+                {0,1, 0 },
+                {-Math.Sin(alpha), 0, Math.Cos(alpha) }
+            };
+
+            _verticiesMatrix= Multiplication(_verticiesMatrix, Mx);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] += totalDeltaX;
+                _verticiesMatrix[i, 1] += totalDeltaY;
+              
+               
+            }
+
+            var result = ConvertMatrixToList(_verticiesMatrix);
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] -= totalDeltaX;
+                _verticiesMatrix[i, 1] -= totalDeltaY;
+            }
+            alpha -= (double)3*Math.PI/180;
+            double[,] Mx =
+            {
+                {Math.Cos(alpha), -Math.Sin(alpha),0 },
+                {Math.Sin(alpha),Math.Cos(alpha), 0 },
+                {0, 0, 1 }
+            };
+
+            _verticiesMatrix= Multiplication(_verticiesMatrix, Mx);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] += totalDeltaX;
+                _verticiesMatrix[i, 1] += totalDeltaY;
+              
+               
+            }
+
+            var result = ConvertMatrixToList(_verticiesMatrix);
+            
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+         
+            _verticiesMatrix = ConvertListToMatrix(_vertices);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] -= totalDeltaX;
+                _verticiesMatrix[i, 1] -= totalDeltaY;
+            }
+            alpha += (double)3*Math.PI/180;
+            double[,] Mx =
+            {
+                {Math.Cos(alpha), -Math.Sin(alpha),0 },
+                {Math.Sin(alpha),Math.Cos(alpha), 0 },
+                {0, 0, 1 }
+            };
+
+            _verticiesMatrix= Multiplication(_verticiesMatrix, Mx);
+            
+            for (int i = 0; i < _verticiesMatrix.GetLength(0); i++)
+            {
+                _verticiesMatrix[i, 0] += totalDeltaX;
+                _verticiesMatrix[i, 1] += totalDeltaY;
+              
+               
+            }
+
+            var result = ConvertMatrixToList(_verticiesMatrix);
+            
+            _vertices.Clear();
+            _vertices = new List<Vertex>(result);
+            pictureBox1.Refresh();
+        }
+    }
+    
+   
+}
